5e69ab49b15c4a4f8424d16c4fa0c48edf3f8c23
Nils Otto Johansen <nils.otto.johansen@husbanken.no>
2016-08-10 19:08:53 +0200

    sisteliste

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from bottle import route, run, static_file
+from bottle import route, run, static_file, request
 import os
 import re
 import glob
@@ -7,19 +7,33 @@ app_path = os.path.dirname(os.path.realpath(__file__ ))
 bilder_path = app_path + "/bilder/"
 godkjent_path = app_path + "/bilder/godkjent/"
 
-@route('/hello')
-def hello():
-    return "Hello World!"
+def hent_bilder(path, antall=1):
+    files = glob.glob(path + "*.JPG")
+    antall = (antall * -1)
+    filnavn = [sorted(files, key=os.path.getctime)[int(antall):]]
+    sisteliste = []
+    for f in filnavn[0]:
+        navnet=os.path.basename(f)
+        print("FILNAVN " + f)
+        print(navnet)
+        m = re.search("[0-9]+", navnet)
+        id = m.group(0).lstrip('0')
+        basepath_ = "/bilde/"
+        if (str.find(path,"godkjent") > -1):
+            basepath_ = basepath_ + "godkjent/"
+        siste = {'path': basepath_ + navnet, "id": id}
+        sisteliste.append(siste)
+    return sisteliste
 
-@route('/siste')
-def siste():
-    files=glob.glob(bilder_path + "*.JPG")
-    filnavn = os.path.basename(sorted(files,key=os.path.getctime)[-1])
-    m = re.search("[0-9]+",filnavn)
-    id = m.group(0).lstrip('0')
-    print(filnavn)
-    siste = {'path': "/bilde/"+filnavn, "id": id}
-    return siste
+@route('/tilgodkjenning')
+def tilgodkjennig():
+    return {"liste": hent_bilder(path=bilder_path)}
+
+@route('/nyeste')
+def nyeste():
+    antall= request.params.antall or 1
+    sisteliste = hent_bilder(path=godkjent_path, antall=antall)
+    return {"liste": sisteliste}
 
 @route('/bilde/<filename:path>')
 def send_static(filename):
